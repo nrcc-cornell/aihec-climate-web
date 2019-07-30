6f68f3cf6db07f791884d000af9360f5b9644f86
Brian Belcher <belcher.bn@gmail.com>
2019-07-30 15:40:09 -0400

    info buttons

---
+++ src/components/tools/Tool1/HelpToolContent.js
@@ -0,0 +1,31 @@
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import Box from '@material-ui/core/Box';
+
+const HelpToolContent = () => {
+        return (
+            <Box padding={1} border={1} borderRadius={4} borderColor="primary.main" bgcolor="#f5f5dc">
+              <p>
+                The <b>Climate Viewer</b> tool allows you to view past, recent and future climate conditions at your nation. Charts are presented by default, and the icon in the upper-right corner of each chart gives you access to tabular, formatted or downloadable data and images. Changes to location, variables and timescales is done to the left of the chart.
+              </p>
+              <p>
+                <b>PAST</b> view shows long-term historical data observed at the selected weather station near your nation. A horizontal black line refers to normal conditions (averages) observed over the period 1981-2010. Colored bars show conditions observed during each available year, relative to the normal.
+              </p>
+              <p>
+                <b>RECENT</b> view shows daily weather observations over the past 3 months (temperature) or season (precipitation) from the selected station near your nation.
+                <ul>
+                  <li>Daily temperature ranges are presented with references to normal and extreme records observed at the site.</li>
+                  <li>Accumulated precipitation for the current year (since January 1) is provided, with reference to normal amounts. </li>
+                </ul>
+              </p>
+              <p>
+                <b>FUTURE</b> view shows averages and ranges of 32 climate models from 1981-2099. Observed values from 1981-2013 are overlayed on the climate model results for reference. These climate models were executed during a joint project in the early part of this century. The blue shading represents years that were simulated by the climate models, while the red shading represents years that were projected by the climate models. <b>NOTE: For nations in Alaska</b>, output from only two climate models are available to view, and each are shown on charts.
+              </p>
+            </Box>
+        );
+}
+
+export default HelpToolContent;
---
+++ src/components/tools/Tool1/HelpUserContent.js
@@ -0,0 +1,40 @@
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import Box from '@material-ui/core/Box';
+
+const HelpUserContent = () => {
+        return (
+            <Box padding={1} border={1} borderRadius={4} borderColor="primary.main" bgcolor="#f5f5dc">
+              <p><b>User input options for the Climate Viewer tool:</b></p>
+              <p><b>CHANGE NATION</b> button allows you to select a specific nation of interest from a map or list.</p>
+              <p><b>STATIONS</b> can be selected from available locations on the map, near your selected nation.</p>
+              <p><b>VARIABLE</b> is the type of weather/climate measurement available to view.
+                <ul>
+                  <li><b>Max Temperature:</b> The highest temperature occurring over an entire day, typically in the afternoon.</li>
+                  <li><b>Min Temperature:</b> The lowest temperature occurring over an entire day, typically early morning.</li>
+                  <li><b>Ave Temperature:</b> The average daily temperature is the average of the day's high and low temperature.</li>
+                  <li><b>Temperature:</b> A combination of daily maximum, minimum and record extremes are included. <i>(RECENT view only)</i></li>
+                  <li><b>Total Precipitation:</b> The total amount of rainfall and melted snowfall received at a location over a chosen period of time.</li>
+                </ul>
+              </p>
+              <p><b>TIMESCALE</b> is the time period over which you would like to view averaged data.
+                <ul>
+                  <li><b>Annual:</b> Daily values are averaged (for temperatures) or summed (for precipitation) over an entire year.</li>
+                  <li><b>Seasonal:</b> Daily values are averaged (for temperatures) or summed (for precipitation) over a selected season.</li>
+                  <li><b>Monthly:</b> Daily values are averaged (for temperatures) or summed (for precipitation) over a selected month.</li>
+                </ul>
+              </p>
+              <p><b>MODEL SCENARIO</b> represents conditions under which the climate models run. <i>(available for FUTURE view only)</i>
+                <ul>
+                  <li><b>High Emissions:</b> Under this scenario, greenhouse gas emissions and concentrations increase considerably over time, with no mitigation.</li>
+                  <li><b>Low Emissions:</b> Under this scenario, greenhouse gas emissions peak at year 2040 and then level off.</li>
+                </ul>
+              </p>
+            </Box>
+        );
+}
+
+export default HelpUserContent;
